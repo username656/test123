c9f0931f3ad88625a47597b57480727101d74e8b
Ionut Ungureanu <ionut.ungureanu@rinftech.com>
2018-04-04 23:56:29 +0300

    KAYAKORW-119 - added password hint

--- service/aurea-boot-autoconfigure/data-user/src/main/resources/data.sql
+++ service/aurea-boot-autoconfigure/data-user/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-//password: secret
+--password=secret
 INSERT INTO users (id, username, password, first_name, last_name, image, status, enabled, reset_key)
 VALUES (1, 'user@example.org', '$2a$10$x5udSzDyTMWOkXG7HRJx5.LpqHayAUkvZ98Gw/aKxiMsdR3gboDJG', 'Default', 'User', '/assets/img/profile-image.jpg', 'ONLINE', true, 'valid-token');
-
