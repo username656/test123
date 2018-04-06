20f3123c5557a05b4c381f39a2327b6d3673d746
Anuj Gupta <anuj78@gmail.com>
2018-04-06 18:37:37 +0530

    jobdsl changes

--- zero-based-clone/jenkins/zbw_jenkins_jobs_cloning.groovy
+++ zero-based-clone/jenkins/zbw_jenkins_jobs_cloning.groovy
@@ -1,13 +1,4 @@
 import hudson.model.*
-import templates.*
-
-//Build build = Executor.currentExecutor().currentExecutable
-//ParametersAction parametersAction = build.getAction(ParametersAction)
-//def buildParams = parametersAction.parameters
-
-//def params = bindings.variables
-//def cIJobCreation = load "templates/ci-build-job.groovy"
-//cIJobCreation.createCIJob("CI Build "+${JENIKINS_PROJECT_NAME}, null)
 
 multibranchPipelineJob("CI Build "+${JENIKINS_PROJECT_NAME}) {
     branchSources {
@@ -77,7 +68,5 @@ multibranchPipelineJob("RC Build "+ ${JENIKINS_PROJECT_NAME}) {
 
 
 
-//IncrementBuildJobTemplate.create("Increment Build "+ ${JENIKINS_PROJECT_NAME}, params)
-//RCBuildJobTemplate.create("RC Build "+ ${JENIKINS_PROJECT_NAME}, params)
 
 
