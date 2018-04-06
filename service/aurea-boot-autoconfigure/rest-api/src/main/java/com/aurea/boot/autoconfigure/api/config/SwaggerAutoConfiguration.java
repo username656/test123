5a80b6c9bd9bc0460c7f01eead44907e657e810e
Vladan Petrovic <vladan.petrovic@jivesoftware.com>
2018-04-06 21:44:25 +0200

    KAYAKORW-154: Swagger UI Data REST adjustment

--- service/aurea-boot-autoconfigure/rest-api/src/main/java/com/aurea/boot/autoconfigure/api/config/SwaggerAutoConfiguration.java
+++ service/aurea-boot-autoconfigure/rest-api/src/main/java/com/aurea/boot/autoconfigure/api/config/SwaggerAutoConfiguration.java
@@ -14,8 +14,10 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,11 +25,13 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 @RequiredArgsConstructor
 public class SwaggerAutoConfiguration {
 
