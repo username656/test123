398e1269684a2f54121b3fd148a72872c2b96192
Ionut Ungureanu <ionut.ungureanu@rinftech.com>
2018-04-09 16:02:16 +0300

    KAYAKORW-153 - fix forgot and reset password backend integration

--- service/aurea-boot-autoconfigure/rest-api-user/src/main/java/com/aurea/boot/autoconfigure/api/user/ApiUserEndpoint.java
+++ service/aurea-boot-autoconfigure/rest-api-user/src/main/java/com/aurea/boot/autoconfigure/api/user/ApiUserEndpoint.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @ApiEndpointMapping(
         value = Mapping.API_USER,
