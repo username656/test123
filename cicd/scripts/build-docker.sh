04a8cfb1df7b74a4faf90f9923517547141ed6ed
bill-yan <36095130+bill-yan@users.noreply.github.com>
2018-07-06 23:35:55 +0800

    Bugfix/jvervis docker not found (#53)
    * create
    
    * cicd
    
    * cicd
    
    * pipelines
    
    * label
    
    * label
    
    * label
    
    * all in one
    
    * fix
    
    * dir
    
    * Jenkinsfile
    
    * jenkins:
      pipeline_jenkinsfile: 'custom/path/to/Jenkinsfile'
    
    * variables
    
    * when
    
    * variables
    
    * jervis
    
    * remove unused variables
    
    * fix branch name 'develop'
    
    * java8-node-docker
    
    * bugfix%252Fjvervis-docker-not-found
    
    * WORKDIR
    
    * WORKDIR
    
    * WORKDIR
    
    * path
    
    * test
    
    * ui
    
    * Ui build back
    
    * fix
    
    * 8u171-node-browsers
    
    * test
    
    * remove test
    
    * clean

--- cicd/scripts/build-docker.sh
+++ cicd/scripts/build-docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 TAG=$1
-BUILDDIR=$2
+WORKDIR=$2
 NAME=$3
 
 #dlb1 to build, dl6 to run
 DOCKER_HOST='tcp://dlb1.aureacentral.com:2375'
 
-echo "TAG=${TAG} NAME=${NAME} DOCKER_HOST=${DOCKER_HOST} BUILDDIR=${BUILDDIR} NG_BUILD_ARGS=${NG_BUILD_ARGS}"
-docker build -t $NAME:$TAG $BUILDDIR
+echo "TAG=${TAG} NAME=${NAME} DOCKER_HOST=${DOCKER_HOST} WORKDIR=${WORKDIR} NG_BUILD_ARGS=${NG_BUILD_ARGS}"
+docker build -t $NAME:$TAG $WORKDIR
